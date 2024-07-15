6a4a2e98bb47f5c2cad1667188f29d1a997bf660
sejmali <sejmali999@gmail.com>
2024-07-15 23:12:14 +0530

    First Commit

---
+++ DSAbeginner/RotateArrayByOne.java
@@ -0,0 +1,21 @@
+// Given an array arr, rotate the array by one position in clock-wise direction.
+
+// Examples:
+
+// Input: arr = [1, 2, 3, 4, 5]
+// Output: [5, 1, 2, 3, 4]
+// Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+// Input: arr = [9, 8, 7, 6, 4, 2, 1, 3]
+// Output: [3, 9, 8, 7, 6, 4, 2, 1]
+// Explanation: After rotating clock-wise 3 comes in first position.
+
+Given an array arr, rotate the array by one position in clock-wise direction.
+
+Examples:
+
+Input: arr = [1, 2, 3, 4, 5]
+Output: [5, 1, 2, 3, 4]
+Explanation: If we rotate arr by one position in clockwise 5 come to the front and remaining those are shifted to the end.
+Input: arr = [9, 8, 7, 6, 4, 2, 1, 3]
+Output: [3, 9, 8, 7, 6, 4, 2, 1]
+Explanation: After rotating clock-wise 3 comes in first position.
